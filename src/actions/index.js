6dd2f8b02035bbc591f7a9891dce0c910ec4f56f
MurrayWarnock <murray.warnock@gmail.com>
2021-07-26 13:30:08 -0600

    axios; first draft actions/reducers

---
+++ src/actions/index.js
@@ -0,0 +1,45 @@
+import axios from 'axios';
+
+export const ADD_PLANT = "ADD_PLANT";
+export const DELETE_PLANT = "DELETE_PLANT";
+export const START_FETCH_PLANTS = "START_FETCH_PLANTS";
+export const SUCCESS_FETCH_PLANTS = "SUCCESS_FETCH_PLANTS";
+export const FAIL_FETCH_PLANTS = "FAIL_FETCH_PLANTS";
+export const SET_VALUE_TO_ERROR_MESSAGE = "SET_VALUE_TO_ERROR_MESSAGE";
+
+export const fetchPlants = () => {
+    return (dispatch) => {
+        dispatch(fetchStart());        
+        axios.get("http://whatever/plants") 
+        .then(resp => {
+            dispatch(fetchSuccess(resp.data));
+        })
+        .catch(err=>{
+            dispatch(fetchFail(err));
+        });
+    }
+}
+
+export const addPlant = (smurf) => {
+    return({type:ADD_PLANT, payload:plant});
+}
+
+export const deletePlant = (id)=>{
+    return({type: DELETE_PLANT, payload:id});
+}
+
+export const fetchStart = ()=> {
+    return({type: START_FETCH_PLANTS});
+}
+
+export const fetchSuccess = (smurfs)=> {
+    return({type: SUCCESS_FETCH_PLANTS, payload:plants});
+}
+
+export const fetchFail = (err)=> {
+    return({type: FAIL_FETCH_PLANTS, payload:err});
+}
+
+export const setError = (err) => {
+    return({type: SET_VALUE_TO_ERROR_MESSAGE, payload:err});
+}
---
+++ src/reducers/index.js
@@ -0,0 +1,52 @@
+import { START_FETCH_PLANTS,
+    SUCCESS_FETCH_PLANTS,   
+    FAIL_FETCH_PLANTS,
+    ADD_PLANT, 
+    DELETE_PLANT,
+    SET_VALUE_TO_ERROR_MESSAGE } from '../actions/plantActions.js';
+
+export const initialState = {
+    plants: [],
+    isLoading: false,
+    dataError: "",
+    errorMessage: "" 
+}
+
+export const plantReducer = (state = initialState, action) => {
+switch(action.type) {
+    case START_FETCH_PLANTS:
+        return({
+            ...state,
+            isLoading: true
+        });
+    case SUCCESS_FETCH_PLANTS:
+        return({
+            ...state,
+            isLoading: false,
+            plants: action.payload
+        });
+    case FAIL_FETCH_PLANTS:
+        return({
+            ...state,
+            dataError: action.type,
+            isLoading: false
+    });                
+    case DELETE_PLANT:
+        return {
+            plants: state.plants.filter(item=>(action.payload !== item.id))
+        }
+    case ADD_PLANT:
+        return {
+            ...state,
+            plants: [...state.plants, action.payload]
+        }
+    case SET_VALUE_TO_ERROR_MESSAGE:
+        return({
+            ...state,
+            errorMessage: action.payload
+        });    
+    default:
+        return state;
+        
+    }
+}
