388d1242b8f4c284e6aa7daeadfea80a6c5428e8
Derek Oh <D.O@Dereks-MacBook-Air.local>
2021-07-26 21:01:20 -0400

    plantlist

---
+++ src/components/Card.js
@@ -0,0 +1,12 @@
+import '../index.css'
+import React from "react";
+export default function Card(){
+
+   return (
+    <div class="card">
+
+        <p>nickname -- species</p>
+    <button class="edit"> Edit </button>
+</div>
+   )
+}
