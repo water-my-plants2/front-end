dcb703fab1f2e051e3f7d07c785e61f1bb0c4fac
Derek Oh <derek.oh17@gmail.com>
2021-07-26 22:04:00 -0400

    plantlist

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,9 +3,16 @@ import React from "react";
 export default function Card(){
 
    return (
-    <div class="card">
-
+    <div class="plant-card">
+        <div class ="box">
+            
+        </div>
         <p>nickname -- species</p>
+        <div class="progress"> 
+            <div>
+                3 days
+            </div>
+        </div>
     <button class="edit"> Edit </button>
 </div>
    )
