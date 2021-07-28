d2ed0da78a596a55749470443f5371b9a46995e5
Marat Kotik <mkotik@Marats-MBP.fios-router.home>
2021-07-28 12:20:18 -0400

    fixed rendering of plant img and h20frequency on plant cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,19 +2,24 @@ import "../index.css";
 import React from "react";
 
 const Card = (props) => {
-  const { plant_nickname, plant_species, h2oFrequency, image } = props.plant;
+  const { plant_nickname, plant_species, h2ofrequency, plant_image } =
+    props.plant;
+
+  // const timer = setInterval(() => console.log(h2oFrequency), 1000);
+  console.log(plant_nickname, h2ofrequency);
+  const count = h2ofrequency;
 
   return (
     <div class="plant-card">
       <div className="plant-inner-wrap">
         <div class="box">
-          <img className="plantimg" src={image}></img>
+          <img className="plantimg" src={plant_image}></img>
         </div>
         <p>{plant_nickname}</p>
         <p>{plant_species}</p>
 
         <div class="progress">
-          <div>{h2oFrequency} days</div>
+          <div>{h2ofrequency} days</div>
         </div>
         <button class="edit"> Edit </button>
       </div>
