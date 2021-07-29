d4d272b68b244bd6bf58b9a672ac996c085f9277
Marat Kotik <mkotik@Marats-MBP.fios-router.home>
2021-07-28 22:15:18 -0400

    added formatting to phone number validation

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { useHistory } from "react-router";
 import { Formik, Form } from "formik";
 import TextField from "./TextField";
@@ -6,19 +6,30 @@ import * as yup from "yup";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function Signup(props) {
-  // const [hasLabel, setHasLabel] = useState(true);
   const history = useHistory();
   const schema = yup.object({
-    username: yup.string().required("Username is Required"),
-    password: yup.string().required("Password is Required"),
+    username: yup
+      .string()
+      .required("Username is Required")
+      .min(3, "Username Must be Over 3 Characters")
+      .max(25, "Username Must be Less Than 25 Characters"),
+    password: yup
+      .string()
+      .required("Password is Required")
+      .min(8, "Password Mus be Over 8 Characters"),
     user_phone: yup
       .string()
       .required("Phone Number is Required")
+      .test("numberFormat", "Must be in the Correct Format", function (value) {
+        if (!value) return true;
+        return value[3] === "-" && value[7] === "-";
+      })
       .max(12, "Character Limit Exceeded"),
     user_email: yup.string().email("Must be a valid email"),
   });
   // if user is loading signup form, remove any existing auth token
   localStorage.removeItem("token");
+
   return (
     <Formik
       initialValues={{
@@ -27,54 +38,56 @@ function Signup(props) {
         user_phone: "",
         user_email: "",
       }}
-      // validationSchema={schema}
-
+      validationSchema={schema}
       onSubmit={(values) => {
         console.log("onSubmit", values);
         props.register(values);
         history.push("plantlist");
       }}
     >
-      {(formik) => (
-        <div className="signup-wrap">
-          <h1>Sign Up</h1>
-          <Form>
-            <TextField
-              label="Username"
-              name="username"
-              type="text"
-              className="signup-textfield"
-            />
-            <TextField
-              label="Password"
-              name="password"
-              type="password"
-              className="signup-textfield"
-            />
-            
-            <TextField
-              label="Phone Number"
-              name="user_phone"
-              type="tel"
-              placeholder="012-345-6789"
-              className="signup-textfield"
-            />
-            <TextField
-              label="Email"
-              name="user_email"
-              type="email"
-              placeholder="optional"
-              className="signup-textfield"
-            />
-            <button type="submit" className="btn-dark">
-              Register
-            </button>
-            <button type="reset" className="btn-danger signup-btn-reset">
-              Reset
-            </button>
-          </Form>
-        </div>
-      )}
+      {(formik) => {
+        return (
+          <div className="signup-wrap">
+            <h1>Sign Up</h1>
+
+            <Form>
+              <TextField
+                label="Username"
+                name="username"
+                type="text"
+                className="signup-textfield"
+              />
+              <TextField
+                label="Password"
+                name="password"
+                type="password"
+                className="signup-textfield"
+              />
+
+              <TextField
+                label="Phone Number"
+                name="user_phone"
+                type="tel"
+                placeholder="012-345-6789"
+                className="signup-textfield"
+              />
+              <TextField
+                label="Email"
+                name="user_email"
+                type="email"
+                placeholder="optional"
+                className="signup-textfield"
+              />
+              <button type="submit" className="btn-dark">
+                Register
+              </button>
+              <button type="reset" className="btn-danger signup-btn-reset">
+                Reset
+              </button>
+            </Form>
+          </div>
+        );
+      }}
     </Formik>
   );
 }
